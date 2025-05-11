290541e553d143d2eb910174778694fc20eee9fb
Jonathan Hussey <jhusseydesign@gmail.com>
2025-05-11 22:25:48 +0400

    feat: Add database and vehicle modules with MongoDB integration
    Introduce a new DatabaseModule with TypeORM configuration for MongoDB, leveraging environment variables for connection settings. Add a VehicleModule along with a Vehicle entity to support vehicle data management. Additionally, update `docker-compose.yml` to use a secure MongoDB root password.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@autonomous/app.controller';
 import { AppService } from '@autonomous/app.service';
 import { ConfigModule } from '@nestjs/config';
+import { VehicleModule } from '@autonomous/vehicle/vehicle.module';
+import { DatabaseModule } from '@autonomous/database/database.module';
 
 @Module({
   imports: [
@@ -9,6 +11,8 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    VehicleModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
