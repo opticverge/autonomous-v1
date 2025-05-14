a994d8da9bbad1173fc4282df8acd031b747b612
Jonathan Hussey <jhusseydesign@gmail.com>
2025-05-15 00:58:51 +0400

    refactor: Refactor database module to enhance configuration and features
    Updated MongoDB connection setup by changing config parameters to use `MONGO_PORT` and `MONGO_HOST` instead of `MONGO_URI`. Added repositories and linked entity classes for improved modularity and functionality with `TypeOrmModule`. Enabled automatic entity loading and logging for better developer experience.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import {
+  VehicleRepository,
+  TelemetryRepository,
+} from '@autonomous/database/repositories';
+import { Telemetry, Vehicle } from '@autonomous/database/entities';
 
 @Module({
   imports: [
@@ -8,13 +13,19 @@ import { ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'mongodb',
-        url: config.getOrThrow<string>('MONGO_URI'),
-        username: config.getOrThrow<string>('MONGO_USERNAME'),
-        password: config.getOrThrow<string>('MONGO_PASSWORD'),
+        port: config.getOrThrow<number>('MONGO_PORT'),
+        host: config.getOrThrow<string>('MONGO_HOST'),
+        username: config.getOrThrow<string>('MONGO_APP_USERNAME'),
+        password: config.getOrThrow<string>('MONGO_APP_PASSWORD'),
         database: config.getOrThrow<string>('MONGO_DATABASE'),
+        autoLoadEntities: true,
         entities: [__dirname + '/../**/*.entity.{ts,js}'],
+        logging: true,
       }),
     }),
+    TypeOrmModule.forFeature([Vehicle, Telemetry]),
   ],
+  providers: [VehicleRepository, TelemetryRepository],
+  exports: [VehicleRepository, TelemetryRepository],
 })
 export class DatabaseModule {}
