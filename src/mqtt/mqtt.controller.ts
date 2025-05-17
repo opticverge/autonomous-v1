2d1066458a0581b6e29ab628701051d56e8afc2a
Jonathan Hussey <jhusseydesign@gmail.com>
2025-05-17 13:59:13 +0400

    feat(mqtt): add handling for vehicle mission status messages
    Introduced a new method to process `VEHICLE_MISSION_STATUS` messages using the `VehicleMissionStatusService`. Updated imports and refactored the controller to include this added functionality.

--- src/mqtt/mqtt.controller.ts
+++ src/mqtt/mqtt.controller.ts
@@ -1,23 +1,26 @@
 import { Controller } from '@nestjs/common';
-import {
-  Ctx,
-  MessagePattern,
-  MqttContext,
-  Payload,
-} from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { TelemetryService } from '@autonomous/mqtt/services';
 import { VehicleTelemetryDto } from 'src/mqtt/dtos';
+import { Topic, VehicleMissionResponse } from '@autonomous/shared/types';
+import { VehicleMissionStatusService } from '@autonomous/vehicle-mission/vehicle-mission-status.service';
 
 @Controller()
 export class MqttController {
+  constructor(
+    private readonly telemetryService: TelemetryService,
+    private readonly vehicleMissionStatusService: VehicleMissionStatusService,
+  ) {}
 
-  constructor(private readonly telemetryService: TelemetryService) {}
-
-  @MessagePattern('vehicle/telemetry')
-  async handleVehicleTelemetry(
-    @Payload() payload: VehicleTelemetryDto,
-    @Ctx() context: MqttContext,
-  ) {
+  @MessagePattern(Topic.VEHICLE_TELEMETRY)
+  async handleVehicleTelemetry(@Payload() payload: VehicleTelemetryDto) {
     await this.telemetryService.process(payload);
+  }
+
+  @MessagePattern(Topic.VEHICLE_MISSION_STATUS)
+  async handleVehicleMissionResponse(
+    @Payload() payload: VehicleMissionResponse,
+  ) {
+    await this.vehicleMissionStatusService.create(payload);
   }
 }
